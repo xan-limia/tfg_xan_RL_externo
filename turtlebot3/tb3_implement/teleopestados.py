c17656bfd7607d891024539951d521490d099e09
xan.limia <xan.limia@udc.es>
2023-04-16 14:47:48 +0200

    separadas as funcionalidades en metodos independientes

--- turtlebot3/tb3_implement/teleopestados.py
+++ turtlebot3/tb3_implement/teleopestados.py
@@ -93,6 +93,36 @@ class TeleoperationNode:
         else:
             return self.stored_velocities[min_idx]
         
+
+    def stop_robot(self):
+        print("No hay coincidencias, denetiendo robot\n")
+        twist = Twist()
+        twist.linear.x = 0.0; twist.linear.y = 0.0; twist.linear.z = 0.0
+        twist.angular.x = 0.0; twist.angular.y = 0.0; twist.angular.z = 0.0
+        node.velocity_publisher.publish(twist)
+
+    def write_images(self):
+        if self.write:
+            if len(self.velocities) > 0 and self.image is not None:
+                now = datetime.datetime.now()
+                print(now)
+                filename = f"{self.folder}/image_{now.strftime('%Y-%m-%d_%H-%M-%S')}.png"
+                filepath = os.path.join(os.getcwd(), filename)
+                cv2.imwrite(filepath, self.image)
+                # Leer imagen
+                img_data = pyexiv2.Image(filepath)
+                metadata = img_data.read_exif()
+                # Agregar metadato
+                metadata['Exif.Photo.UserComment'] = f"linear={self.target_linear_vel}\nangular={self.target_angular_vel}"
+                img_data.modify_exif(metadata)
+                self.stored_images.append(self.image)
+                self.stored_velocities.append((self.target_linear_vel, self.target_angular_vel))
+                print("salvados", len(self.stored_images))
+            self.img_msg = None
+            self.image = None
+            self.velocities = []
+            self.write = False
+        
     def teleop(self):
         self.load_images()
         while not rospy.is_shutdown():
@@ -110,11 +140,8 @@ class TeleoperationNode:
 
                     self.velocity_publisher.publish(twist)
                 else:
-                    print("No hay coincidencias, denetiendo robot\n")
-                    twist = Twist()
-                    twist.linear.x = 0.0; twist.linear.y = 0.0; twist.linear.z = 0.0
-                    twist.angular.x = 0.0; twist.angular.y = 0.0; twist.angular.z = 0.0
-                    node.velocity_publisher.publish(twist)
+                    self.stop_robot()
+
                     key = input(msg)
                     if key.lower() == 'w':
                         self.target_linear_vel = 0.05
@@ -147,26 +174,7 @@ class TeleoperationNode:
                     self.velocities.append(twist)
                     self.velocity_publisher.publish(twist)
 
-                    if self.write:
-                        if len(self.velocities) > 0 and self.image is not None:
-                            now = datetime.datetime.now()
-                            print(now)
-                            filename = f"{self.folder}/image_{now.strftime('%Y-%m-%d_%H-%M-%S')}.png"
-                            filepath = os.path.join(os.getcwd(), filename)
-                            cv2.imwrite(filepath, self.image)
-                            # Leer imagen
-                            img_data = pyexiv2.Image(filepath)
-                            metadata = img_data.read_exif()
-                            # Agregar metadato
-                            metadata['Exif.Photo.UserComment'] = f"linear={self.target_linear_vel}\nangular={self.target_angular_vel}"
-                            img_data.modify_exif(metadata)
-                            self.stored_images.append(self.image)
-                            self.stored_velocities.append((self.target_linear_vel, self.target_angular_vel))
-                            print("salvados", len(self.stored_images))
-                        self.img_msg = None
-                        self.image = None
-                        self.velocities = []
-                        self.write = False
+                    
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
@@ -177,9 +185,6 @@ if __name__ == '__main__':
     node = TeleoperationNode(foldername)
     node.teleop()
 
-    twist = Twist()
-    twist.linear.x = 0.0; twist.linear.y = 0.0; twist.linear.z = 0.0
-    twist.angular.x = 0.0; twist.angular.y = 0.0; twist.angular.z = 0.0
-    node.velocity_publisher.publish(twist)
+    node.stop_robot()
     
     
